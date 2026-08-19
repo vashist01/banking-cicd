fac55c0219f070a11eff8529017ba61e33576971
vashist01 <rajesh.vashist.01@gmail.com>
2026-08-19 17:14:44 +0530

    update iam.tf file with add role

--- demo/terraform/iam.tf
+++ demo/terraform/iam.tf
@@ -22,10 +22,9 @@ resource "aws_iam_role" "ecs_task_execution_role" { ##AWS mein IAM Role create k
     Enviroment = "dev"
     ManagedBy  = "terraform"
   }
-}
 
+}
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"  
-
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
