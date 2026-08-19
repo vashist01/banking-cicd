80b0d03dc7dedf42a86f2693b68a0a854447cbe4
vashist01 <rajesh.vashist.01@gmail.com>
2026-08-19 18:25:07 +0530

    manage jenkins and terrform both

--- demo/terraform/task-definition.tf
+++ demo/terraform/task-definition.tf
@@ -1,21 +1,21 @@
 resource "aws_ecs_task_definition" "demo" {
-  faimly                   = "demo"
-  required_campatibilities = ["FARGATE"] # Matlab:ECS task Fargate par chalega.
+  family                   = "demo"
+  requires_compatibilities = ["FARGATE"] # Matlab:ECS task Fargate par chalega.
   # hume EC2 server manage nahi karna padega.
 
-  network_model = "awsvpc" #Fargate ke liye important hai. Task ko apna network interface/IP milta hai.
+  network_mode = "awsvpc" #Fargate ke liye important hai. Task ko apna network interface/IP milta hai.
 
 
   cpu    = "256" #Initial demo ke liye small allocation.
   memory = "512"
 
-  execution_role_arn = aws_iam_role.ecs_task_definition_role.arn #Terraform automatically dependency samajh lega:
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn #Terraform automatically dependency samajh lega:
   #Yani IAM role pehle create hoga.
 
-  container_definition = jsonencode([
+  container_definitions = jsonencode([
     {
       name  = "demo"
-      image = "${aws_ecr_repository.demo.repoisoty_url}:latest" 
+      image = "${aws_ecr_repository.demo.repository_url}:latest"
       #Terraform ECR repository ka URL automatically use karega.Lekin production mein latest ideal nahi hai
       #demo:${GIT_COMMIT_SHA} because immutable version tracking/rollback easy hota hai.
 
