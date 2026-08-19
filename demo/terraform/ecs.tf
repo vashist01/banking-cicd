148186a34ed904a9b0393c87c3816beb8343407d
vashist01 <rajesh.vashist.01@gmail.com>
2026-08-19 17:40:55 +0530

    update resource for permission in iam.tf file

---
+++ demo/terraform/ecs.tf
@@ -0,0 +1,9 @@
+resource "aws_ecs_cluster" "demo" { # AWS mein ECS Cluster create karo.
+  name = "banking-cluster"
+
+  tags = {
+    Project    = "banking-cicd"
+    Enviroment = "dev"
+    ManagedBy  = "terraform"
+  }
+}
