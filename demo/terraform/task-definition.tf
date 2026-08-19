d2fd0945819a121521d4081646298d72883c4930
vashist01 <rajesh.vashist.01@gmail.com>
2026-08-19 18:06:10 +0530

    task-definition

---
+++ demo/terraform/task-definition.tf
@@ -0,0 +1,38 @@
+resource "aws_ecs_task_definition" "demo" {
+  faimly                   = "demo"
+  required_campatibilities = ["FARGATE"] # Matlab:ECS task Fargate par chalega.
+  # hume EC2 server manage nahi karna padega.
+
+  network_model = "awsvpc" #Fargate ke liye important hai. Task ko apna network interface/IP milta hai.
+
+
+  cpu    = "256" #Initial demo ke liye small allocation.
+  memory = "512"
+
+  execution_role_arn = aws_iam_role.ecs_task_definition_role.arn #Terraform automatically dependency samajh lega:
+  #Yani IAM role pehle create hoga.
+
+  container_definition = jsonencode([
+    {
+      name  = "demo"
+      image = "${aws_ecr_repository.demo.repoisoty_url}:latest" 
+      #Terraform ECR repository ka URL automatically use karega.Lekin production mein latest ideal nahi hai
+      #demo:${GIT_COMMIT_SHA} because immutable version tracking/rollback easy hota hai.
+
+
+      essentinal = true
+      portMapping = [{
+
+        containerPort = 8008 #par chalti hai,
+        protocol      = "tcp"
+        }
+      ]
+    }
+  ])
+
+  tags = {
+    Project    = "banking-cicd"
+    Enviroment = "dev"
+    ManagedBy  = "terraform"
+  }
+}
