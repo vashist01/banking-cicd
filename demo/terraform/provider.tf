664fe3e9d599a888fc6a193c111bc32195ce3b75
vashist01 <rajesh.vashist.01@gmail.com>
2026-08-19 15:37:06 +0530

    correct terraform file formate

--- demo/terraform/provider.tf
+++ demo/terraform/provider.tf
@@ -1,12 +1,12 @@
-tarraform {
+terraform {
     required_providers {
         aws ={
-            source ="hashcorp/aws"
+            source ="hashicorp/aws"
             verison = "~>6.0"
         }
     }
 }
-required_version =  ">=1.6.0"
+required_version = ">= 1.6.0"
 
 #AWS infrastructure manage karne ke liye AWS provider use karo.
 
